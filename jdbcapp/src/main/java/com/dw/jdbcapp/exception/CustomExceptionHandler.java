65fb8c54696d3b3769288e715e86e8131d365822
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-12-19 14:24:52 +0900

    add

--- jdbcapp/src/main/java/com/dw/jdbcapp/exception/CustomExceptionHandler.java
+++ jdbcapp/src/main/java/com/dw/jdbcapp/exception/CustomExceptionHandler.java
@@ -2,6 +2,7 @@ package com.dw.jdbcapp.exception;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,6 +22,13 @@ public class CustomExceptionHandler {
             InvalidRequestException e) {
         return new ResponseEntity<>(
                 e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(
+            ResourceNotFoundException e) {
+        return new ResponseEntity<>(
+                e.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
 

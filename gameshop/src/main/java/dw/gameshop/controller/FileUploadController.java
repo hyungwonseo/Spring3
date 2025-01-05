0b8e57485d393fc7452194946d21b0e76bed7819
hwseo12 <hwseo@naver.com>
2025-01-05 17:09:36 +0900

    file upload

--- gameshop/src/main/java/dw/gameshop/controller/FileUploadController.java
+++ gameshop/src/main/java/dw/gameshop/controller/FileUploadController.java
@@ -1,15 +1,19 @@
 package dw.gameshop.controller;
 
+import dw.gameshop.model.User;
+import dw.gameshop.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -19,33 +23,50 @@ import java.nio.file.Paths;
 public class FileUploadController {
     @Value("${file.upload-dir}")
     private String uploadDir;
+    @Autowired
+    UserService userService;
 
     @PostMapping("/upload")
-    public String uploadFile(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
         try {
+            // 현재 세션의 userName을 사용하여 세션별로 폴더 저장위치를 구별함
+            User currentUser = userService.getCurrentUser(request);
             // 업로드 폴더확인, 없으면 생성
-            Path uploadPath = Paths.get(uploadDir);
+            Path uploadPath = Paths.get(uploadDir, currentUser.getUserName());
             if (!Files.exists(uploadPath)) {
                 Files.createDirectories(uploadPath);
             }
+            // 동일파일이 존재하면 "파일이름_1.확장자" 방식으로 카운트를 증가시켜서 중복저장을 회피함
+            String originalFileName = file.getOriginalFilename();
+            String fileName = originalFileName;
+            int counter = 1;
+            while (Files.exists(uploadPath.resolve(fileName))) { // 파일 이름 중복 검사
+                int dotIndex = originalFileName.lastIndexOf(".");
+                String name = dotIndex == -1 ? originalFileName : originalFileName.substring(0, dotIndex);
+                String extension = dotIndex == -1 ? "" : originalFileName.substring(dotIndex);
+                fileName = name + "_" + counter + extension;
+                counter++;
+            }
             // 파일이름을 저장위치와 조합
-            Path copyLocation = uploadPath.resolve(file.getOriginalFilename()).normalize();
+            Path copyLocation = uploadPath.resolve(fileName).normalize();
             // 파일저장
             Files.copy(file.getInputStream(), copyLocation);
-            return "File uploaded successfully: " + file.getOriginalFilename();
-        } catch (FileAlreadyExistsException e) {
-            e.printStackTrace();
-            return "File already exist";
+            return new ResponseEntity<>(
+                    "File uploaded successfully: " + fileName,
+                    HttpStatus.OK);
         } catch (IOException e) {
-            e.printStackTrace();
-            return "Failed to upload file";
+            return new ResponseEntity<>(
+                    "Failed to upload file",
+                    HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @GetMapping("/download/{fileName}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName) {
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
         try {
-            Path filePath = Paths.get(uploadDir).resolve(fileName).normalize();
+            // 현재 세션의 userName을 사용하여 세션별로 폴더 저장위치를 구별함
+            User currentUser = userService.getCurrentUser(request);
+            Path filePath = Paths.get(uploadDir, currentUser.getUserName()).resolve(fileName).normalize();
             Resource resource = new UrlResource(filePath.toUri());
 
             if (resource.exists() || resource.isReadable()) {
@@ -57,7 +78,7 @@ public class FileUploadController {
                 return ResponseEntity.notFound().build();
             }
         } catch (Exception e) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.internalServerError().build();
         }
     }
 }

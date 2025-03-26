70bce09db7e5d35f903f2f3a56307366d339c149
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-03-26 16:10:29 +0900

    Update ChatController.java

--- gameshop_jwt/src/main/java/dw/gameshop/chat/ChatController.java
+++ gameshop_jwt/src/main/java/dw/gameshop/chat/ChatController.java
@@ -24,6 +24,7 @@ public class ChatController {
             SimpMessageHeaderAccessor headerAccessor
     ) {
         // Add username in web socket session
+        // 연결종료시 누구의 연결이 종료된 것인지 확인하기 위해 저장
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;
     }
